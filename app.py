fe2df6182037e9bec838b15e5ece677a4703736c
ftokoro <f.tokoro@mbm.me.es.osaka-u.ac.jp>
2023-06-22 13:35:00 +0900

    add app

--- app.py
+++ app.py
@@ -4,6 +4,26 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
+@app.message('hello')
+def message_hello(message, say):
+    say(
+        blocks=[
+        {
+            "type": "section",
+            "text": {"type": "mrkdwn",
+                     "text":f"Hey there <@{message['user']}>!"},
+            "accessory": {
+                "type":"button",
+                "text":{"type": "plain_text",
+                        "text": "Click Me"},
+                "action_id":"button_click"
+            }
+        }
+    ],
+    text=f'Hey there <@{message["user"]}>!'
+
+    )
+
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
     handler.start()
